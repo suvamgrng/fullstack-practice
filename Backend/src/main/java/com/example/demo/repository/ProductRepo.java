e9e2809b2d76f769572dc43ab67f12e033458b55
suvamgurung <gurungsuvam265@gmail.com>
2026-06-21 15:47:14 +0545

    Update: Removed unwanted code in ProductRepo and Added Update & Delete Button

--- Backend/src/main/java/com/example/demo/repository/ProductRepo.java
+++ Backend/src/main/java/com/example/demo/repository/ProductRepo.java
@@ -4,5 +4,5 @@ import com.example.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Integer> {
-    Integer id(int id);
+
 }
